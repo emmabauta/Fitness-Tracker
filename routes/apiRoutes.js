ca30b239bb66020339a48d1501148d5d4918248e
DillEmma <emmabauta@gmail.com>
2021-02-18 18:22:04 -0500

    added get, post, and put routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,36 @@
 const router = require('express').Router();
 const Workout = require('../models/workout');
 
-
-// Get Route 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
-  .then(Workout => {
-    res.json(Workout);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  }); 
+  .then((Workout) => {
+    res.json(Workout)
+    })
+  .catch((err) => {
+    res.json(err);
+  });
 });
-router.post("/api/workouts", (req, res) => {
-  Workout.find({})
-  .then(Workout => {
-    res.json(Workout);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  }); 
-}); 
-router.put("/api/workouts/:id", (req, res) => {
-  Workout.find({})
-  .then(Workout => {
-    res.json(Workout);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  }); 
-}); 
+router.post('/api/workouts', (req, res) => {
+  Workout.create({})
+    .then((Workout) => {
+    res.json(Workout)
+    })
+  .catch((err) => {
+    res.json(err);
+  });
+});
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    {$push: {exercises: body}},
+    {new: true, runValidators: true})
+    .then((Workout) => {
+      res.json(Workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    }); 
+});
+
 //Aggregate function for get routes when combining data from get routes for updating workout
 module.exports = router;
